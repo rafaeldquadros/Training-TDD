c1e9f56ac44974aa3e6f05e51a07d922d6ff927e
Rafael Quadros <contatorafaelquadros@gmail.com>
2023-07-03 15:51:17 -0300

    methods for model purchase order

--- purchase_orders/model.py
+++ purchase_orders/model.py
@@ -9,3 +9,20 @@ class PurchaseOrderModel(db.Model):
 
     def __init__(self, description):
         self.description = description
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__}
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()  # select * from purchase_order
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(
+            id=_id
+        ).first()  # select * from purchase_order where id = _id
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
